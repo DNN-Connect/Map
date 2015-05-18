90741f673906bcaff60fb466b10137d5775d24b0
Peter Donker <peter@bring2mind.net>
2015-05-19 00:33:09 +0200

    Add the DAL2

---
+++ Models/MapPoints/MapPoint.cs
@@ -0,0 +1,35 @@
+using System.Runtime.Serialization;
+using DotNetNuke.ComponentModel.DataAnnotations;
+
+namespace Connect.DNN.Modules.Map.Models.MapPoints
+{
+
+    [TableName("vw_Connect_Map_MapPoints")]
+    [PrimaryKey("MapPointId", AutoIncrement = true)]
+    [Scope("ModuleId")]
+    [DataContract]
+    public class MapPoint : MapPointBase
+    {
+
+        #region Public Properties
+        [DataMember]
+        public string CreatedByUser { get; set; }
+        [DataMember]
+        public string LastModifiedByUser { get; set; }
+        #endregion
+
+        #region Public Methods
+        public MapPointBase GetMapPointBase()
+        {
+            MapPointBase res = new MapPointBase();
+            res.MapPointId = MapPointId;
+            res.ModuleId = ModuleId;
+            res.Latitude = Latitude;
+            res.Longitude = Longitude;
+            res.Message = Message;
+            return res;
+        }
+        #endregion
+
+    }
+}
