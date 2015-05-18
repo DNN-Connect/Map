fa02fee7240eba1b997401fe05cb35154d47b2ac
Peter Donker <peter@bring2mind.net>
2015-05-19 00:51:00 +0200

    Status and error handling in WebAPI to UI

--- js/Connect.Map.js
+++ js/Connect.Map.js
@@ -5,16 +5,19 @@ function ConnectMapService($, settings, mid) {
 	var baseServicepath = $.dnnSF(moduleId).getServiceRoot('Connect/Map');
 
 	this.ajaxCall = function (type, controller, action, id, data, success, fail) {
+		showLoading();
 		$.ajax({
 			type: type,
 			url: baseServicepath + controller + '/' + action + (id != null ? '/' + id : ''),
 			beforeSend: $.dnnSF(moduleId).setModuleHeaders,
 			data: data
 		}).done(function (retdata) {
+			hideLoading();
 			if (success != undefined) {
 				success(retdata);
 			}
 		}).fail(function (xhr, status) {
+			showError(xhr.responseText);
 			if (fail != undefined) {
 				fail(xhr.responseText);
 			}
@@ -24,5 +27,27 @@ function ConnectMapService($, settings, mid) {
 	this.getDataPoints = function (success) {
 		this.ajaxCall('GET', 'MapPoints', 'List', null, null, success);
 	}
+}
 
+function showLoading() {
+	if ($('#mapStatus').length) {
+		$('#mapStatus div:first-child').show();
+		$('#mapStatus div:nth-child(2)').hide();
+		$('#mapStatus').css('background', '#2FC1F3').show();
+	}
+}
+
+function hideLoading() {
+	if ($('#mapStatus').length) {
+		$('#mapStatus').hide();
+	}
+}
+
+function showError(message) {
+	if ($('#mapStatus').length) {
+		$('#mapStatus div:first-child').hide();
+		$('#mapStatus div:nth-child(2)').html(message).show();
+		$('#mapStatus').css('background', '#F33B2F').show();
+		setTimeout(function () { $('#cmStatus').hide(); }, 3000);
+	}
 }
