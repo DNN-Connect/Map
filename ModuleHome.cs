938028fe0035fbd63f9f9115f66fc999c54fc83a
Peter Donker <peter@bring2mind.net>
2015-05-19 01:19:35 +0200

    Finishing touches

--- ModuleHome.cs
+++ ModuleHome.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using Connect.DNN.Modules.Map.Common;
+using DotNetNuke.Collections;
 
 namespace Connect.DNN.Modules.Map
 {
 
     public class ModuleHome : ModuleBase
     {
+        public string View { get; set; }
+
         protected override string RazorScriptFile
         {
             get
             {
-                return "~/DesktopModules/Connect/Map/Views/Home.cshtml";
+                return string.Format("~/DesktopModules/Connect/Map/Views/{0}.cshtml", View);
             }
         }
 
         protected void Page_Init(object sender, EventArgs e)
         {
+            View = Request.QueryString.GetValueOrDefault<string>("View", Settings.View);
             AddService();
             LocalResourceFile = Globals.SharedResourceFileName;
         }
