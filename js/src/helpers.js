e23f1f4ec81c820d4e635c087a3df9dd42f545c2
Peter Donker <peter@bring2mind.net>
2015-09-19 20:17:21 +0200

    Fix to slide panel

--- js/src/helpers.js
+++ js/src/helpers.js
@@ -1,32 +1,30 @@
 var ConnectMapHelpers = (function($) {
-    return {
-        slidePanel: function(panel) {
-            if (panel.css('display') == 'block') {
-                $('body').off("click");
-                panel.off("click");
-                panel.animate({
-                    right: -window.innerWidth
-                }, 800, function() {
-                    panel.css('display', 'none');
-                    $('body').css('overflow', 'auto');
-                });
-            } else {
-                $('body').css('overflow', 'hidden');
-                panel.css('right', -window.innerWidth);
-                panel.css('display', 'block');
-                panel.animate({
-                    right: 0
-                }, 800);
-                var that = this;
-                $('body').on("click", function(e) {
-                    that.slidePanel(panel);
-                });
-                panel.on("click", function(e) {
-                    e.stopPropagation();
-                })
-            }
-        }
+  return {
+    slidePanel: function(panel) {
+      if (panel.css('display') == 'block') {
+        $('body').off("click");
+        panel.animate({
+          right: -window.innerWidth
+        }, 800, function() {
+          panel.css('display', 'none');
+          $('body').css('overflow', 'auto');
+        });
+      } else {
+        $('body').css('overflow', 'hidden');
+        panel.css('right', -window.innerWidth);
+        panel.css('display', 'block');
+        panel.animate({
+          right: 0
+        }, 800);
+        var that = this;
+        $('body').on("click", function(e) {
+          if ($(e.target).closest('#' + panel[0].id).length == 0) {
+            that.slidePanel(panel);
+          }
+        });
+      }
     }
+  }
 })(jQuery);
 
 module.exports = ConnectMapHelpers;
