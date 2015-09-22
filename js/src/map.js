95562655ef58d732a39cd4f18547fd17712b85d3
Peter Donker <peter@bring2mind.net>
2015-09-22 17:05:56 +0200

    Asynchronous loading of google api

--- js/src/map.js
+++ js/src/map.js
@@ -1,16 +1,49 @@
 /** @jsx React.DOM */
-var ConnectMap = require('./ConnectMap'),
-    ConnectMapHelpers = require('./helpers')
-    ConnectMapSettings = require('./ConnectMapSettings');
+var ConnectMapComponent = require('./ConnectMap');
 
-(function($) {
-    $(document).ready(function() {
-        if($('#connectMapPanel').length == 0) {
-            $('body').append('<div id="connectMapPanel" class="connectMapPanel"></div>');
-        };
-        $('.connectMap').each(function(i, el) {
-            var moduleId = $(el).data('moduleid');
-            React.render(<ConnectMap moduleId={moduleId} />, el);
-        });
-    });
-}(jQuery));
+
+
+;
+(function($, window, document, undefined) {
+
+  $(document).ready(function() {
+    if ($('#connectMapPanel').length == 0) {
+      $('body').append('<div id="connectMapPanel" class="connectMapPanel"></div>');
+    };
+    var googleScript = 'http://maps.googleapis.com/maps/api/js?callback=ConnectMap.loadMaps';
+    if ($('.connectMap').first().data('googlekey') !== '') {
+      googleScript += '&key=' + $('.connectMap').first().data('googlekey');
+    }
+    loadScript(googleScript);
+  });
+
+  window.loadScript = function(src, callback) {
+    var s,
+      r,
+      t;
+    r = false;
+    s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = src;
+    s.onload = s.onreadystatechange = function() {
+      if (!r && (!this.readyState || this.readyState == 'complete')) {
+        r = true;
+        if (callback !== undefined) {
+          callback();
+        }
+      }
+    };
+    t = document.getElementsByTagName('script')[0];
+    t.parentNode.insertBefore(s, t);
+  }
+
+  window.ConnectMap = {
+    loadMaps: function() {
+      $('.connectMap').each(function(i, el) {
+        var moduleId = $(el).data('moduleid');
+        React.render(<ConnectMapComponent moduleId={moduleId} />, el);
+      });
+    }
+  }
+
+})(jQuery, window, document);
